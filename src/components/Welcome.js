6acd5d9791a4ae9977f542e8fc4837ec89dcd9aa
Ritwik880 <rittiksinha12@gmail.com>
2021-08-07 22:35:14 +0530

    fixing data

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -17,13 +17,17 @@ const Welcome = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <div class="p-5 mb-4 bg-light rounded-3">
-                <div class="container-fluid py-5">
-                    <h1 class="display-5 fw-bold">Hello User!</h1>
-                    <p class="para col-md-8 fs-4">We are so glad that you have passed our signup verification.</p>
-                    <button class="btn btn-primary btn-lg" type="button">Great</button>
+            <div className="container1">
+                <div class="p-5 mb-4 bg-light rounded-3">
+                    <div class="container-fluid py-5">
+                        <h1 class="display-5 fw-bold">Hello User!</h1>
+                        <p class="para col-md-8 fs-4">We are so glad that you have passed our signup verification.</p>
+                        <button class="btn btn-primary btn-lg" type="button">Great</button>
+                    </div>
                 </div>
+
             </div>
+
             <footer>
                 <p>All Rights Reserved</p>
             </footer>
